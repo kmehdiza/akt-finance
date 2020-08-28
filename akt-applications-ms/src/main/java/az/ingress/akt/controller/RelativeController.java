df545a8e76263af288d0a81ddc8c63bc82657f0e
atbakirov <atbakirov@gmail.com>
2020-08-28 15:15:50 +0400

    Added testing for RelativeService

--- akt-applications-ms/src/main/java/az/ingress/akt/controller/RelativeController.java
+++ akt-applications-ms/src/main/java/az/ingress/akt/controller/RelativeController.java
@@ -3,6 +3,7 @@ package az.ingress.akt.controller;
 import az.ingress.akt.dto.RelativeDto;
 import az.ingress.akt.service.RelativeService;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,9 +11,10 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
 @Controller
+@RequiredArgsConstructor
 public class RelativeController {
 
-    private RelativeService relativeService;
+    private final RelativeService relativeService;
 
     @PostMapping(value = "/debtor/relatives", consumes = {MediaType.APPLICATION_JSON_VALUE,
             MediaType.MULTIPART_FORM_DATA_VALUE})
